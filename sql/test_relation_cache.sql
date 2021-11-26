9685ed6cfb9283b2f1f2c6ca41236416ac8d6bae
hzhang2 <hzhang2@vmware.com>
2021-11-25 19:30:27 -0500

    add sql and output for unit test

--- sql/test_relation_cache.sql
+++ sql/test_relation_cache.sql
@@ -9,5 +9,17 @@ WITH table_size AS (
 SELECT (a).* FROM table_size order by table_size;
 
 select pg_table_size('t') as ts from gp_dist_random('gp_id') order by ts;
+abort;
 
+begin;
+create table t(t text);
+insert into t select array(select * from generate_series(1,1000)) from generate_series(1, 1000);
+
+WITH table_size AS (
+    SELECT diskquota.diskquota_fetch_table_stat(1, ARRAY['t'::regclass]) AS a
+    FROM  gp_dist_random('gp_id')
+)
+SELECT (a).* FROM table_size order by table_size;
+
+select pg_table_size('t') as ts from gp_dist_random('gp_id') order by ts;
 abort;
