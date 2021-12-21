6ee3cbbf7b0d9ca8a7c762ea3279f905aa86a43c
Zhang Hao <1446384557@qq.com>
2021-12-21 14:43:41 +0800

    Fix test case bug (#114)
    The SQL statement is not equal to the expected output in test_vacuum.sql.
    
    Co-authored-by: hzhang2 <hzhang2@vmware.com>

--- tests/regress/sql/test_vacuum.sql
+++ tests/regress/sql/test_vacuum.sql
@@ -13,7 +13,7 @@ INSERT INTO b SELECT generate_series(1,10);
 DELETE FROM a WHERE i > 10;
 VACUUM FULL a;
 SELECT pg_sleep(20);
-SELECT tableid::regclass, size, segid from diskquota.table_size WHERE tableid::regclass::name not like '%.%' ORDER BY size, segid DESC;
+SELECT tableid::regclass, size, segid from diskquota.table_size WHERE tableid::regclass::name NOT LIKE '%.%' ORDER BY size, segid DESC;
 
 -- expect insert succeed
 INSERT INTO a SELECT generate_series(1,10);
